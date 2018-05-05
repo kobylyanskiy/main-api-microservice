b2ff9727b35fb76419f80be4448c2d08a107eaaf
Kirill Kobylyanskiy <kirill.kobylyanskiy@emlid.com>
2018-05-05 21:26:46 +0300

    CD: authorize gcloud

--- update-service.sh
+++ update-service.sh
@@ -6,6 +6,15 @@ VERSION=$1
 PROJECT_ID=spy-crowd
 
 ${DOCKER} build -t ${IMAGE_LOCAL_PREFIX}/${DEPLOYMENT}:${VERSION} .
+
+echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode -i > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+gcloud --quiet config set project $PROJECT_NAME_PRD
+gcloud --quiet config set container/cluster $CLUSTER_NAME_PRD
+gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
+
 ${DOCKER} tag ${IMAGE_LOCAL_PREFIX}/${DEPLOYMENT}:${VERSION} gcr.io/${PROJECT_ID}/${DEPLOYMENT}:${VERSION}
 ${DOCKER} push gcr.io/${PROJECT_ID}/${DEPLOYMENT}:${VERSION}
 
