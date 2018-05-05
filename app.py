5b8671c8c3f22af9ca54531fe01c5c23b330874e
Kirill Kobylyanskiy <kirill.kobylyanskiy@emlid.com>
2018-05-05 19:07:10 +0300

    app: send request to mongo api

--- app.py
+++ app.py
@@ -9,9 +9,43 @@ app = Flask(__name__)
 @app.route('/agents', methods=['GET', 'POST'])
 def agents():
     if request.method == 'POST':
-        return requests.post('http://mongo-api:5000/agents').content
+        agent = request.get_json(silent=True)
+        response = requests.post('http://mongo-api:5000/agents', data=agent).json()
+        return json.dumps({
+            'result': response['result'],
+        })
     else:
-        return requests.get('http://mongo-api:5000/agents').content
+        response = requests.get('http://mongo-api:5000/agents').json()
+        if response['result']:
+            return json.dumps({
+                'result': True,
+                'data': [],
+                'codefresh': 'test',
+            })
+        else:
+            return json.dumps({
+                'result': False,
+                'data': [],
+                'codefresh': 'test',
+            })
+
+
+@app.route('/agents/<string:codename>', methods=['GET', 'POST'])
+def get_agent(codename):
+    if request.method == 'POST':
+        agent = request.get_json(silent=True)
+        response = requests.post('http://mongo-api:5000/agents/{}'.format(codename), data=agent).json()
+        return json.dumps({'result': response['result']})
+    else:
+        response = requests.get('http://mongo-api:5000/agents/{}'.format(codename)).json()
+        if not response['result']:
+            return json.dumps({
+                'result': False,
+            })
+        return json.dumps({
+            'result': True,
+            'agent': response['agent'],
+        })
 
 
 if __name__ == '__main__':
