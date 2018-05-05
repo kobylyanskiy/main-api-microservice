bc29137db6d58c3e72cf23769fb9aea36d7f29fe
Kirill Kobylyanskiy <kirill.kobylyanskiy@emlid.com>
2018-05-05 21:31:35 +0300

    CD: authorize gcr

--- update-service.sh
+++ update-service.sh
@@ -16,6 +16,9 @@ gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
 
 ${DOCKER} tag ${IMAGE_LOCAL_PREFIX}/${DEPLOYMENT}:${VERSION} gcr.io/${PROJECT_ID}/${DEPLOYMENT}:${VERSION}
+
+docker login -u _json_key --password-stdin https://gcr.io < ${HOME}/gcloud-service-key.json
+
 ${DOCKER} push gcr.io/${PROJECT_ID}/${DEPLOYMENT}:${VERSION}
 
 ${KUBECTL} set image deployment/${DEPLOYMENT}-deployment ${DEPLOYMENT}=gcr.io/${PROJECT_ID}/${DEPLOYMENT}:${VERSION}
